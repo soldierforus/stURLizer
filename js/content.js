622c67f2f18bd1726b882bbcb9835e85575fd65b
anon <anon@pop-os.localdomain>
2022-05-23 05:58:03 -0700

    fixes redeclaration error

--- js/content.js
+++ js/content.js
@@ -1,18 +1,18 @@
-//get the current url and remove the params
-let diseasedURL = ''
-let sterilizedURL = ''
-
-if (diseasedURL == '') {
+(() => {
   //get the current url and remove the params
-  diseasedURL = window.location.href;
-  sterilizedURL = diseasedURL.split('?')[0];
-  diseasedURL = '';
-  
-  //create the data to be written to the the clipboard
-  const type = "text/plain";
-  const blob = new Blob([sterilizedURL], { type });
-  const data = [new ClipboardItem({ [type]: blob })];
-  
-  //write to the clipboard
-  navigator.clipboard.write(data);
-}
+  let diseasedURL = ''
+  let sterilizedURL = ''
+
+    //get the current url and remove the params
+    diseasedURL = window.location.href;
+    sterilizedURL = diseasedURL.split('?')[0];
+    diseasedURL = '';
+    
+    //create the data to be written to the the clipboard
+    const type = "text/plain";
+    const blob = new Blob([sterilizedURL], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    
+    //write to the clipboard
+    navigator.clipboard.write(data);
+})();
